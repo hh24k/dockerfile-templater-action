fbfff41b46f9cc411b0db061315bdce61645f21f
Hai He <hai.he@enterprisedb.com>
2023-10-20 10:40:58 +0800

    chore: support a few more dockerfile-templater args
    Signed-off-by: Hai He <hai.he@enterprisedb.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ usage() {
 # e.g. INPUT_DOCKERFILE_TPL is the value of the dockerfile_tpl input
 # see https://help.github.com/en/articles/metadata-syntax-for-github-actions#example
 # Parse and validate the inputs
-
+echo "Running templater with the version: $(dockerfile-templater --version)"
 if test -n "$INPUT_*"; then
 
 		# set the required options
@@ -22,22 +22,31 @@ if test -n "$INPUT_*"; then
         echo "ERROR: No dockerfile variants specified."
         usage
     fi
-		set -- "--dockerfile.tpl" $INPUT_DOCKERFILE_TPL "--variants.def" $INPUT_VARIANTS_DEF
+		set -- "--dockerfile.tpl" "$INPUT_DOCKERFILE_TPL" "--variants.def" "$INPUT_VARIANTS_DEF"
 
 		# set the optional options
 		if test -n "$INPUT_VARIANTS_CFG"; then
-				set -- "$@" "--variants.cfg" "$INPUT_VARIANTS_CFG"
+			set -- "$@" "--variants.cfg" "$INPUT_VARIANTS_CFG"
 		fi
 
 		if test -n "$INPUT_OUT_DIR"; then
-				set -- "$@" "--out.dir" "$INPUT_OUT_DIR"
+			set -- "$@" "--out.dir" "$INPUT_OUT_DIR"
 		fi
-
+		# TODO: support dockerfile.var as stringArray
 		if test -n "$INPUT_DOCKERFILE_TPL_DIR"; then
-				set -- "$@" "--dockerfile.tpldir" "$INPUT_DOCKERFILE_TPL_DIR"
+			set -- "$@" "--dockerfile.tpldir" "$INPUT_DOCKERFILE_TPL_DIR"
 		fi
+		# TODO: support dockerfile.var as stringToString
+		if test -n "$INPUT_DOCKERFILE_VAR"; then
+			set -- "$@" "--dockerfile.var" "$INPUT_DOCKERFILE_VAR"
+    fi
 
-		echo "$@"
+    if test -n "$INPUT_VARIANTS_CFG"; then
+    		set -- "$@" "--variants.cfg" "$INPUT_VARIANTS_CFG"
+    fi
+
+		echo "Running dockerfile-templater with inputs:  $*"
+
 		templater "$@"
 else
 		echo "ERROR: No inputs specified."
